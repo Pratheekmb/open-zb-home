7fd489da06f54932555fe7bd217fbee773b5f3f9
ruZZ iL <ruzzmo@gmail.com>
2011-08-27 23:23:52 +0300

    misc styling and formating. changed color schememisc styling and formating. changed color scheme..

--- Site/assets/color_picker.js
+++ Site/assets/color_picker.js
@@ -4,8 +4,7 @@
 	Event.onDOMReady(function() {
 			YAHOO.log("Creating Color Picker.", "info", "example");
 			picker = new YAHOO.widget.ColorPicker("light_color_picker", {
-					showhsvcontrols: true,
-					showhexcontrols: true,
+
 					images: {
 						PICKER_THUMB: "assets/picker_thumb.png",
 						HUE_THUMB: "assets/hue_thumb.png"
@@ -28,13 +27,6 @@
 														 //wil fire; true would silence it
 			});
 			
-			//use the "get" method to get the current value
-			//of one of the Color Picker's properties; in 
-			//this case, we'll get the hex value and write it
-			//to the log:
-			Event.on("gethex", "click", function(e) {
-				sendCmd("2*"+picket.get("hex"));
-			});
 		});
 })();
 	
